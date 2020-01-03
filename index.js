ed36c2652b437bdbcb0c77f4f15e31a35ccf9ddb
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-01-03 16:08:53 +0300

    добавил корс

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-require("dotenv").config();
-
 const Koa = require("koa");
 const app = new Koa();
+const koaCors = require("koa-cors");
 
+require("dotenv").config();
 require("./db");
 
 const koaBody = require("koa-body");
 const router = require("./router");
 
 app.use(koaBody());
-app.use(router.routes());
-app.use(router.allowedMethods());
+app.use(koaCors());
+app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(3000, () => {
-  console.log("http://localhost:3000");
+app.listen(8080, () => {
+  console.log("http://localhost:8080");
 });
