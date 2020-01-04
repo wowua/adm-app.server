3aa5c6e0ab5dfd620c0c06df8fbd71d589be2985
Vladimir Sabantsev <vladimir.sabantsev@gmail.com>
2020-01-04 15:45:01 +0300

    временно убрал проверку на userid в вишах

--- api/wishes.js
+++ api/wishes.js
@@ -2,15 +2,14 @@ const Wish = require("../models/wish");
 
 exports.add = ({ title }) =>
   new Promise(async (resolve, reject) => {
-    try {
-      if (!title) {
-        resolve({
-          success: false,
-          message: "title is required"
-        });
-        return;
-      }
+    if (!title) {
+      return resolve({
+        success: false,
+        message: "title is required"
+      });
+    }
 
+    try {
       const newWish = new Wish({
         title
       });
@@ -23,5 +22,26 @@ exports.add = ({ title }) =>
       });
     } catch (err) {
       reject(err);
+    }
+  });
+
+exports.get = ({ userId }) =>
+  new Promise(async (resolve, reject) => {
+    // if (!userId) {
+    //   return resolve({
+    //     success: false,
+    //     message: "userId is required"
+    //   });
+    // }
+
+    try {
+      const wishes = await Wish.find({ userId: 1 });
+
+      resolve({
+        success: true,
+        wishes
+      });
+    } catch (error) {
+      reject(error);
     }
   });
